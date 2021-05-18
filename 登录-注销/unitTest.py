cc51a48b8cd76eddb8184ee920033ad87be2c103
zszdxasdf <70731264+zszdxasdf@users.noreply.github.com>
2021-05-18 09:45:13 +0800

    冲

---
+++ 登录-注销/unitTest.py
@@ -0,0 +1,252 @@
+import time
+import unittest
+
+from selenium.webdriver.common.by import By
+
+import HTMLTestReport
+import tool
+
+class unitTest(unittest.TestCase):
+    # 登录失败-账号不存在
+    def test1(self):
+        '''登录失败-账号不存在测试'''
+        print("========【case_0001】登录失败-账号不存在测试 =============")
+        b = tool.login("1885774837", "xr952343147")
+        tool.wait(2)
+        addUnitMsg = b.find_element_by_xpath("//div[@role='alert']/p").text
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+        self.assertEqual('账号不存在!', addUnitMsg)
+        tool.close(b)
+
+    # 登录失败-密码错误
+    def test2(self):
+        '''登录失败-密码错误测试'''
+        print("========【case_0002】登录失败-密码错误测试 =============")
+        b = tool.login("18857748370", "xr")
+        tool.wait(2)
+        addUnitMsg = b.find_element_by_xpath("//div[@role='alert']/p").text
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+        self.assertEqual('账号或密码不正确!', addUnitMsg)
+        tool.close(b)
+
+    # 登录成功-注销
+    def test3(self):
+        '''登录成功-注销测试'''
+        print("========【case_0003】登录成功-注销测试 =============")
+        b = tool.login("18857748370", "xr952343147")
+        tool.wait(2)
+        b.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/div[2]/div[4]/div/div/i').click()
+        tool.wait(1)
+        elements = b.find_elements_by_class_name("el-dropdown-menu__item")
+        elements[4].click()
+        tool.wait(1)
+        addUnitMsg = b.find_element_by_xpath("//div[@role='alert']/p").text
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+        self.assertEqual('注销成功', addUnitMsg)
+        tool.close(b)
+
+    # 首页配置-设置平台模板
+    def test4(self):
+        '''首页配置-设置平台模板测试'''
+        print("========【case_0004】首页配置-设置平台模板测试 =============")
+        b = tool.login("18857748370", "xr952343147")
+        tool.wait(1)
+        b.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/div[2]/div[4]/div/div/i').click()
+        tool.wait(2)
+        elements = b.find_elements_by_class_name("el-dropdown-menu__item")
+        elements[2].click()
+        tool.wait(2)
+        b.find_element(By.XPATH, '//*[@role="tabpanel"]/div[1]/div[1]/div[1]/div[1]').click()
+        tool.wait(2)
+        b.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div/div/div/div/div/div[2]/button/span').click()
+        tool.wait(2)
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+        tool.close(b)
+
+    # 首页配置-搜索存在模板
+    def test5(self):
+        '''首页配置-搜索存在模板试'''
+        print("========【case_0005】首页配置-搜索存在模板测试 =============")
+        b = tool.login("18857748370", "xr952343147")
+        tool.wait(1)
+        b.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/div[2]/div[4]/div/div/i').click()
+        tool.wait(2)
+        elements = b.find_elements_by_class_name("el-dropdown-menu__item")
+        elements[2].click()
+        tool.wait(2)
+        tool.setSearch(b,"空模板")
+        tool.wait(2)
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+        tool.close(b)
+
+    # 首页配置-搜索不存在模板
+    def test6(self):
+        '''首页配置-搜索不存在模板测试'''
+        print("========【case_0006】首页配置-搜索不存在模板测试 =============")
+        b = tool.login("18857748370", "xr952343147")
+        tool.wait(1)
+        b.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/div[2]/div[4]/div/div/i').click()
+        tool.wait(2)
+        elements = b.find_elements_by_class_name("el-dropdown-menu__item")
+        elements[2].click()
+        tool.wait(2)
+        tool.setSearch(b,"啥")
+        tool.wait(2)
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+        tool.close(b)
+
+    # 首页配置-自定义模板
+    def test7(self):
+        '''首页配置-自定义模板测试'''
+        print("========【case_0007】首页配置-自定义模板测试 =============")
+        b = tool.login("18857748370", "xr952343147")
+        tool.wait(1)
+        b.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/div[2]/div[4]/div/div/i').click()
+        tool.wait(2)
+        elements = b.find_elements_by_class_name("el-dropdown-menu__item")
+        elements[2].click()
+        tool.wait(2)
+        b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[4]/button/span').click()
+        tool.wait(1)
+        b.find_element_by_xpath("//div[contains(text(),'待办事项')]").click()
+        tool.wait(1)
+        b.find_element_by_xpath("//div[contains(text(),'资产卡片')]").click()
+        tool.wait(1)
+        b.find_element_by_xpath("//div[contains(text(),'预警按钮')]").click()
+        tool.wait(2)
+        b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/button[2]/span').click()
+        tool.wait(2)
+        b.find_element_by_xpath("//span[contains(text(),'新增')]").click()
+        tool.wait(1)
+        tool.setText(b,'name',"自定义模板1")
+        tool.wait(1)
+        b.find_element_by_xpath(
+            '//*[@id="app"]/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/span/button[1]/span').click()
+        tool.wait(1)
+        addUnitMsg = b.find_element_by_xpath("//div[@role='alert']/p").text
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+        self.assertEqual('添加成功', addUnitMsg)
+        tool.close(b)
+
+    # 首页配置-设置自定义模板
+    def test8(self):
+        '''首页配置-设置自定义模板测试'''
+        print("========【case_0008】首页配置-设置自定义模板测试 =============")
+        b = tool.login("18857748370", "xr952343147")
+        tool.wait(1)
+        b.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/div[2]/div[4]/div/div/i').click()
+        tool.wait(2)
+        elements = b.find_elements_by_class_name("el-dropdown-menu__item")
+        elements[2].click()
+        tool.wait(2)
+        b.find_element(By.XPATH, "//div[contains(text(),'自定义模板1')]").click()
+        tool.wait(2)
+        b.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div/div/div/div/div/div[2]/button/span').click()
+        tool.wait(2)
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+        tool.close(b)
+
+    # 编辑单位信息-区划名称（待补）
+    def test9(self):
+        '''新增土地啥都不写测试'''
+        print("========【case_0001】新增土地啥都不写测试 =============")
+        b = tool.login("13588631227", "Toby0901")
+        tool.joinItem(b, "单位管理", "单位信息")
+        # b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[2]/div[1]/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/form/div/div/div/div/div/div/button/i').click()
+        # b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div[2]/div[1]/form/div[2]/div/div[2]/div/div/div/input').send_keys(value)
+        # b.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/button').click()
+        # 点击编辑单位信息
+        b.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/button').click()
+
+        #相当于setObject（）
+        b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div[2]/div[1]/form/div[6]/div/div[2]/div/div/div/div/input').send_keys("辽宁省")
+        tool.wait(1)
+        b.find_element_by_xpath("//div[@x-placement='bottom-start']/div[1]/div[1]/ul/li/div/span[text()='" + value + "']").click()
+        tool.wait(1)
+
+        b.find_element(By.XPATH, '//button/span[text()="完成"]').click()
+        tool.wait(2)
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+
+        tool.close(b)
+
+    # 编辑单位信息-单位地址（待补）
+    def test9(self):
+        '''新增土地啥都不写测试'''
+        print("========【case_0001】新增土地啥都不写测试 =============")
+        b = tool.login("13588631227", "Toby0901")
+        tool.joinItem(b, "单位管理", "单位信息")
+        # b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[2]/div[1]/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/form/div/div/div/div/div/div/button/i').click()
+        # b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div[2]/div[1]/form/div[2]/div/div[2]/div/div/div/input').send_keys(value)
+        # b.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/button').click()
+        # 点击编辑单位信息
+        b.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/button').click()
+
+        #相当于setObject（）
+        b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div[2]/div[1]/form/div[6]/div/div[2]/div/div/div/div/input').send_keys("辽宁省")
+        tool.wait(1)
+        b.find_element_by_xpath("//div[@x-placement='bottom-start']/div[1]/div[1]/ul/li/div/span[text()='" + value + "']").click()
+        tool.wait(1)
+
+        b.find_element(By.XPATH, '//button/span[text()="完成"]').click()
+        tool.wait(2)
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+
+        tool.close(b)
+
+    # 创建集团-集团名称未填
+    def test10(self):
+        '''新增土地啥都不写测试'''
+        print("========【case_0001】新增土地啥都不写测试 =============")
+        b = tool.login("13588631227", "Toby0901")
+        tool.joinItem(b, "单位管理", "单位信息")
+        # b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[2]/div[1]/div/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/form/div/div/div/div/div/div/button/i').click()
+        # b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div[2]/div[1]/form/div[2]/div/div[2]/div/div/div/input').send_keys(value)
+        # b.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/button').click()
+        # 点击创建集团
+        b.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div[1]/div[2]/div/button[3]').click()
+
+        #相当于setText()
+        b.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div[2]/div/form/div[1]/div/div[1]/input').send_keys("我dio任要吃屎")
+        tool.wait(1)
+        b.find_element_by_xpath("//div[@x-placement='bottom-start']/div[1]/div[1]/ul/li/div/span[text()='" + value + "']").click()
+        tool.wait(1)
+
+        b.find_element(By.XPATH, '//button/span[text()="完成"]').click()
+        tool.wait(2)
+        current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+        pic_path = '.\\unittest\\' + current_time + '.png'  # 设置存储图片路径，测试结果图片可以按照每天进行区分
+        print(pic_path)
+        b.save_screenshot(pic_path)  # 截图，获取测试结果
+
+        tool.close(b)
---
+++ 首页配置/zmain.py
@@ -0,0 +1,41 @@
+import time
+import unittest
+
+import HTMLTestReport
+import unitTest
+if __name__ == "__main__":
+    '''生成测试报告'''
+
+    current_time = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+    testunit = unittest.TestSuite()
+
+    #testunit.addTest(unitTest.unitTest("test1"))
+    #testunit.addTest(unitTest.unitTest("test2"))
+    #testunit.addTest(unitTest.unitTest("test3"))
+    testunit.addTest(unitTest.unitTest("test4"))
+    testunit.addTest(unitTest.unitTest("test5"))
+    testunit.addTest(unitTest.unitTest("test6"))
+    testunit.addTest(unitTest.unitTest("test7"))
+    testunit.addTest(unitTest.unitTest("test8"))
+    # testunit.addTest(landTest.landTest("test9"))
+    # testunit.addTest(landTest.landTest("test10"))
+    # testunit.addTest(landTest.landTest("test11"))
+    # testunit.addTest(landTest.landTest("test12"))
+    # testunit.addTest(landTest.landTest("test13"))
+    # testunit.addTest(landTest.landTest("test14"))
+    # testunit.addTest(landTest.landTest("test15"))
+    # testunit.addTest(landTest.landTest("test16"))
+    # testunit.addTest(landTest.landTest("test17"))
+    # testunit.addTest(landTest.landTest("test18"))
+    # testunit.addTest(landTest.landTest("test19"))
+    # testunit.addTest(landTest.landTest("test20"))
+    # testunit.addTest(landTest.landTest("test21"))
+    # testunit.addTest(landTest.landTest("test22"))
+    # testunit.addTest(landTest.landTest("test23"))
+    # testunit.addTest(landTest.landTest("test24"))
+    report_path = ".\\test\\" + current_time + '.html'  # 生成测试报告的路径
+    fp = open(report_path, "wb")
+    runner = HTMLTestReport.HTMLTestRunner(stream=fp, title=u"自动化测试报告", description='自动化测试演示报告', tester='童峻涛')
+    runner.run(testunit)
+    fp.close()
+    print("完成："+report_path)
